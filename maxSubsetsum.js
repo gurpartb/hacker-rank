b399e3a67745a1ad5a7ef8de0e9c70c3314ca539
gurpartb <gurpartap.s.bhatti@gmail.com>
2019-03-24 22:56:37 -0600

    cleaned up the code

--- maxSubsetsum.js
+++ maxSubsetsum.js
@@ -11,8 +11,8 @@ function maxSubsetSum(arr) {
 
     for(let i = arr.length - 3; i >= 0; i--){
 
-        maxarr[i] = Math.max(arr[i], arr[i] + maxarr[i+2],
-        arr[i] + maxarr[i+3], maxarr[i+2], maxarr[i+3]);
+        maxarr[i] = Math.max(arr[i], arr[i] + maxarr[i + 2],
+        arr[i] + maxarr[i + 3], maxarr[i + 2], maxarr[i + 3]);
     }
     
     return Math.max(maxarr[0], maxarr[1]);
