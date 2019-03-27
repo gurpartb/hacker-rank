b1f8f41480acc7b9eff12997df644a81b4e2e024
gurpartb <gurpartap.s.bhatti@gmail.com>
2019-03-27 10:54:31 -0600

    implemented array.prototype.count method

--- repeatedString.js
+++ repeatedString.js
@@ -1,25 +1,34 @@
-// Complete the repeatedString function below.
-function repeatedString(s, n) {
-    // number of times 'a' occurs in 's
-    let as = 0;
-    let letter = 'a'
-    s.split('').forEach(el=>{
-        if(el === letter){
-            as += 1;
+Array.prototype.count = function(str){
+    let c = 0;
+
+    this.forEach(el=>{
+        if(el === str){
+            c += 1;
         }
     });
-    // number of time s occurs completely in n
-    let sn = Math.floor(n/s.length);
-    // partial size of s in n
-    let r = n%s.length;
+
+    return c;
+}
+
+// Complete the repeatedString function below.
+function repeatedString(s, n) {
+    // matching pattern
+    let letter = 'a'
+
+    // number of times 'a' occurs in 's
+    let as = s.split('').count(letter);
+
+    // number of time s occurs 'quotient' completely in n
+    let sn = ~~(n/s.length);
+
+    // partial size 'remainder' of s in n
+    let r = n % s.length;
+
     // # of times 'a' ocurrs in s.substring(0,r)
-    let ar = 0;
-    s.substring(0,r).split('').forEach(el=>{
-        if(el === letter){
-            ar += 1;
-        }
-    })
+    let ar = s.substring(0,r).split('').count(letter)
+
     // total occurences of 'a'
     let at = as*sn+ar;
+
     return at;
 }
