ef7372531c7bcf83b41977bb0784eded63f47a65
gurpartb <gurpartap.s.bhatti@gmail.com>
2019-04-05 12:25:30 -0600

    finished ransomNote challenge

---
+++ ransomNote.js
@@ -0,0 +1,31 @@
+Array.prototype.counter = function(){
+
+    let obj = {}; 
+
+    this.forEach( el =>{
+
+        if(obj[el]){
+
+            obj[el]++;
+        }
+        else {
+
+            obj[el] = 1;
+        }
+    })
+
+    return obj;
+}
+
+function checkMagazine(magazine, note) {
+
+    let magCount = magazine.counter();
+    let noteCount = note.counter();
+
+    let isSubSet = Object.keys( noteCount ).every( el =>{
+
+        return (magCount[el] && noteCount[el] <= magCount[el]);
+    })
+
+    console.log(isSubSet? 'Yes':'No');
+}
