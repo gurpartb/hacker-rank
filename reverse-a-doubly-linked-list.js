d0705da572e14b3034a0ddc9d5fdecb00abf42e4
gurpartb <gurpartap.s.bhatti@gmail.com>
2019-04-08 16:52:05 -0600

    finished reverse challenge

---
+++ reverse-a-doubly-linked-list.js
@@ -0,0 +1,26 @@
+//    ____    ____
+// ->|prev|->|curr|->
+// <-|____|<-|____|<-
+//
+function reverse(head) {
+
+    let curr = head;
+    // initially prev is undefined
+    let prev;
+
+    // loops stops when current is undefined
+    while(curr){
+
+        // switch the properties of the current node
+        curr.prev = curr.next;
+        curr.next = prev;
+
+        // since prev was initially undefined,
+        // simply set prev to curr node to move is forward
+        prev = curr;
+        curr = curr.prev;
+    }
+
+    // curr points to undefined, prev points to the last valid node (new head)
+    return prev;
+}
