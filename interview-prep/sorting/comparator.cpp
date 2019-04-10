fb1926b8b6f570d3cac0bd7e02cc55740bf5d0b8
gurpartb <gurpartap.s.bhatti@gmail.com>
2019-04-09 20:00:55 -0600

    cleaned up the code

--- interview-prep/sorting/comparator.cpp
+++ interview-prep/sorting/comparator.cpp
@@ -1,30 +1,28 @@
 #include <string>
 
-struct Player
-{
+struct Player {
     int score;
     std::string name;
 };
 
-
 static int comparator(Player a, Player b){
         
-        // ascending order
-        if(a.score > b.score){
+    // ascending order
+    if(a.score > b.score){
 
-            return 1;
-        }
-
-        if(a.score < b.score){
-
-            return -1;
-        }
-
-        // decending order
-        if(a.name > b.name){
-
-            return -1;
-        }
-        
         return 1;
     }
+
+    if(a.score < b.score){
+
+        return -1;
+    }
+
+    // decending order
+    if(a.name > b.name){
+
+        return -1;
+    }
+    
+    return 1;
+}
