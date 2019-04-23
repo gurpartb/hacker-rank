e2cf564d4aa9eb120638c9acdfaf9eb4e8200f07
gurpartb <gurpartap.s.bhatti@gmail.com>
2019-04-22 22:50:34 -0600

    cleaned up the code circularArrayRotation

--- problem-solving/algorithm/easy/circularArrayRotation.js
+++ problem-solving/algorithm/easy/circularArrayRotation.js
@@ -1,11 +1,11 @@
 function circularArrayRotation(a, k, queries) {
 
     let res = new Array(queries.length)
-    let rotated = new Array(a.length)
 
     for(let i = 0; i < res.length; i++){
 
-        let j = ( ( queries[i] - k ) %  a.length + a.length) % a.length;
+        let j =  ( queries[i] - (k %  a.length) + a.length ) % a.length;
+
         res[i] = a[j];
     }
     return res;
