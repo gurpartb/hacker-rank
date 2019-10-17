04bdd8654cfb2ecbfefbf58f2908bea9f7b2aa81
Gurpartap BHATTI <gurpartap.bhatti@amadeus.com>
2019-10-17 11:10:43 -0600

    another day another challenge

--- SQL/thePads.sql
+++ SQL/thePads.sql
@@ -2,7 +2,6 @@ SELECT concat (Name, '(', left( Occupation, 1), ')')
 FROM Occupations
 ORDER BY Name;
 SELECT concat( 'There are a total of ', count( Occupation), ' ', lower( left( Occupation, 1) ), substring( Occupation, 2, len(Occupation) ), 's.' ) 
---[occupation]s.count(Occupation) as OccupationCount, Occupation
 FROM Occupations 
 GROUP BY Occupation
 ORDER BY count( Occupation), Occupation;
